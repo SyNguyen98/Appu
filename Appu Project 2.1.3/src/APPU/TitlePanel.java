bd99fd306567960f10ddac5bcd61d3bc5e91f7c5
SyNguyen98 <nghsynguyen98@gmail.com>
2019-03-27 14:30:36 +0700

    Update TitlePanel.java

--- Appu Project 2.1.3/src/APPU/TitlePanel.java
+++ Appu Project 2.1.3/src/APPU/TitlePanel.java
@@ -10,7 +10,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
 import javax.imageio.ImageIO;
 import javax.swing.GroupLayout;
