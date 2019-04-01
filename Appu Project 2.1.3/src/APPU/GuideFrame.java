7bd8eb29afceb0f59778ebb53bc108f734960398
SyNguyen98 <48832411+SyNguyen98@users.noreply.github.com>
2019-04-01 23:16:20 +0700

    Create GuideFrame.java

---
+++ Appu Project 2.1.3/src/APPU/GuideFrame.java
@@ -0,0 +1,19 @@
+package APPU;
+
+import java.io.IOException;
+import javax.imageio.ImageIO;
+import javax.swing.JFrame;
+
+public class GuideFrame extends JFrame {
+
+    public GuideFrame() {
+        setTitle("Guide");
+        setSize(500, 500);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        try {
+            setIconImage(ImageIO.read(TitlePanel.class.getResource("/Pictures/Icon.png")));
+        } catch (IOException ex) {}
+        
+    }
+}
