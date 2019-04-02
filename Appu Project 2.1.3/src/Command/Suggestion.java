a45694341dbf641ea526bb990deda0bad22ef03c
SyNguyen98 <nghsynguyen98@gmail.com>
2019-04-02 14:59:26 +0700

    Create Suggestion.java

---
+++ Appu Project 2.1.3/src/Command/Suggestion.java
@@ -0,0 +1,12 @@
+package Command;
+
+import javax.swing.JPopupMenu;
+
+public class Suggestion extends JPopupMenu {
+
+    public Suggestion() {
+        add("#video");
+        add("#picture");
+    }
+    
+}
