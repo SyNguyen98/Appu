d0ec1630b624cc7179cbf79ae3b9de72eb19dc01
SyNguyen98 <nghsynguyen98@gmail.com>
2019-04-03 07:50:37 +0700

    Update MainFrame.java

--- Appu Project 2.1.3/src/APPU/MainFrame.java
+++ Appu Project 2.1.3/src/APPU/MainFrame.java
@@ -21,7 +21,7 @@ public class MainFrame  {
         try {
             UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
         } catch(ClassNotFoundException | IllegalAccessException | InstantiationException | UnsupportedLookAndFeelException e) {}
-        Theme.play("src/Sound/EndGame.wav");
+        Theme.play("src/Sound/nokia.wav");
         
         introLabel.setIcon(new ImageIcon(MainFrame.class.getResource("/Pictures/Intro.gif")));
         introLabel.setBounds(0, 0, 800, 640);
