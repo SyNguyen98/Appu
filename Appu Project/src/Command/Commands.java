a259efc82901ea3c1a131ab97e38e601aceb7f27
SyNguyen98 <48832411+SyNguyen98@users.noreply.github.com>
2019-04-08 23:25:32 +0700

    Update Commands.java

--- Appu Project/src/Command/Commands.java
+++ Appu Project/src/Command/Commands.java
@@ -97,5 +97,13 @@ public class Commands {
             }
             online = false;
         }
+        if(command.contains("data structure")) {
+            try {
+                desktop.open(new File("src/Database/visualization.jar"));
+            } catch (IOException e) {
+                System.out.println(e.getMessage());
+            }
+            online = false;
+        }
     }
 }
