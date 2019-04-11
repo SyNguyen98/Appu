0de7b0ebfa4204c7eb9126e79b2a62521f3163e0
SyNguyen98 <48832411+SyNguyen98@users.noreply.github.com>
2019-04-11 21:23:00 +0700

    Create Computer.java

---
+++ Appu Project/src/Command/Computer.java
@@ -0,0 +1,80 @@
+package Command;
+
+import MainFrame.InputPanel;
+import java.awt.Desktop;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+public class Computer {
+    private static final Desktop desktop = Desktop.getDesktop();
+    private static final JSONParser parser = new JSONParser();
+    public static boolean online;
+    
+    private static String readDatabase(String command) { 
+        try {
+            JSONArray jsonArray = (JSONArray) parser.parse(new FileReader("src/Database/ControlPC.json"));
+            for (Object obj : jsonArray) {                 
+                JSONObject jsonObject = (JSONObject) obj;               
+                String key = (String) jsonObject.get("Key");
+                if(command.contains(key)) {
+                    return (String) jsonObject.get("Command");
+                }
+            }
+        } catch (FileNotFoundException e) {
+        } catch (IOException | ParseException e) {}
+        return null;
+    }
+    
+    public static void controlComputer() {
+        String command = InputPanel.inputField.getText();
+        Runtime runtime = Runtime.getRuntime();
+        Process proc = null;
+        
+        String control = readDatabase(command);
+        if (control != null) {
+            try {
+                proc = runtime.exec(control);
+            } catch (IOException ex) {}
+            System.exit(0);
+            online = false;
+        }
+        if(command.contains("open word")) {
+            try {
+                desktop.open(new File("C:\\Program Files (x86)\\Microsoft Office\\root\\Office16\\WINWORD.exe"));
+            } catch (IOException e) {
+                System.out.println(e.getMessage());
+            }
+            online = false;
+        }
+        if(command.contains("open powerpoint")) {
+            try {
+                desktop.open(new File("C:\\Program Files (x86)\\Microsoft Office\\root\\Office16\\POWERPNT.exe"));
+            } catch (IOException e) {
+                System.out.println(e.getMessage());
+            }
+            online = false;
+        }
+        if(command.contains("open excel")) {
+            try {
+                desktop.open(new File("C:\\Program Files (x86)\\Microsoft Office\\root\\Office16\\EXCEL.exe"));
+            } catch (IOException e) {
+                System.out.println(e.getMessage());
+            }
+            online = false;
+        }
+        if(command.contains("data structure")) {
+            try {
+                desktop.open(new File("src/Database/visualization.jar"));
+            } catch (IOException e) {
+                System.out.println(e.getMessage());
+            }
+            online = false;
+        }
+    }
+}
