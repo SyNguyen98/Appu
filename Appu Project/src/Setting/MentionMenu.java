f07c83e8d46a12e140b2f7d44d802fc4da929f98
SyNguyen98 <48832411+SyNguyen98@users.noreply.github.com>
2019-04-17 02:38:02 +0700

    Update MentionMenu.java

--- Appu Project/src/Setting/MentionMenu.java
+++ Appu Project/src/Setting/MentionMenu.java
@@ -1,7 +1,21 @@
 package Setting;
 
+import java.awt.Color;
+import java.awt.Font;
+import javax.swing.JMenuItem;
 import javax.swing.JPopupMenu;
 
-public class MentionMenu extends JPopupMenu {
 
+public class MentionMenu extends JPopupMenu {
+    private static JMenuItem video, picture, map, music, translate;
+    
+    public MentionMenu() {
+        video = new JMenuItem("#video");
+        video.setFont(new Font("Arial", 1, 12));
+        video.setBackground(new Color(0, 0, 51));
+        video.setForeground(Color.WHITE);
+        add(video);
+        setOpaque(false);
+        setBorderPainted(false);
+    }
 }
