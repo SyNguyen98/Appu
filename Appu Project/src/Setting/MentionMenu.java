c8979868993aad0a82a92b85346795708e0a08b0
SyNguyen98 <48832411+SyNguyen98@users.noreply.github.com>
2019-04-17 20:08:26 +0700

    Update MentionMenu.java

--- Appu Project/src/Setting/MentionMenu.java
+++ Appu Project/src/Setting/MentionMenu.java
@@ -2,20 +2,30 @@ package Setting;
 
 import java.awt.Color;
 import java.awt.Font;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import javax.swing.JMenuItem;
 import javax.swing.JPopupMenu;
 
 
-public class MentionMenu extends JPopupMenu {
-    private static JMenuItem video, picture, map, music, translate;
-    
+public class MentionMenu extends JPopupMenu {  
     public MentionMenu() {
-        video = new JMenuItem("#video");
-        video.setFont(new Font("Arial", 1, 12));
-        video.setBackground(new Color(0, 0, 51));
-        video.setForeground(Color.WHITE);
-        add(video);
-        setOpaque(false);
-        setBorderPainted(false);
+        try (BufferedReader br = new BufferedReader(new FileReader("src/Database/KeyAutoComplete.txt"))) {
+            String line = br.readLine(); 
+            while (line != null) {
+                JMenuItem item = new JMenuItem();     
+                item.setOpaque(false);
+                item.setText(line);
+                item.setFont(new Font("Arial", 1, 12));
+                item.setBackground(new Color(0, 0, 51));
+                item.setForeground(Color.WHITE);
+                item.setHorizontalTextPosition(JMenuItem.LEFT);
+                add(item);           
+                line = br.readLine();
+            }
+        } catch (FileNotFoundException ex) {
+        } catch (IOException ex) {}
     }
 }
