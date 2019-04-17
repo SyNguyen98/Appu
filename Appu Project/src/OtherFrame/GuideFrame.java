e57e7b97d696ef802f0cd1d59f2cdd905c976418
Tùng Phan Thanh <48848418+thanhtung2105@users.noreply.github.com>
2019-04-17 22:06:27 +0700

    Update GuideFrame.java

--- Appu Project/src/OtherFrame/GuideFrame.java
+++ Appu Project/src/OtherFrame/GuideFrame.java
@@ -3,16 +3,23 @@ package OtherFrame;
 import MainFrame.TitlePanel;
 import java.io.IOException;
 import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 public class GuideFrame extends JFrame {
+    private static final JLabel GUIDE = new JLabel();
 
     public GuideFrame() {
         setTitle("Guide");
-        setSize(500, 500);
+        setSize(540, 550);
         setLocationRelativeTo(null);
         try {
             setIconImage(ImageIO.read(TitlePanel.class.getResource("/Pictures/Icon.png")));
         } catch (IOException ex) {}
+        GUIDE.setIcon(new ImageIcon(GuideFrame.class.getResource("/Pictures/1.gif")));
+        GUIDE.setBounds(0, 0, 50, 50);
+        add(GUIDE);
+  //      GUIDE.setVisible(false);
     }
 }
