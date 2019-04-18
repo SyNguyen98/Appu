4e8b834e6dfd59a2ddd780ab5f6f87c6e172b1b0
SyNguyen98 <nghsynguyen98@gmail.com>
2019-04-18 11:15:07 +0700

    Update GuideFrame.java

--- Appu Project/src/OtherFrame/GuideFrame.java
+++ Appu Project/src/OtherFrame/GuideFrame.java
@@ -5,16 +5,14 @@ import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.io.IOException;
 import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 public class GuideFrame extends JFrame {
-<<<<<<< HEAD
     private JLabel GUIDE = new JLabel();
     private JLabel StartTurial = new JLabel();
     private JLabel StartImg = new JLabel();
- //   private static final JButton StarTurial = new JButton();
-=======
->>>>>>> d743ceeaef39a635ba55946d575c96c2423bee63
 
     public GuideFrame() {  
         setResizable(false);
@@ -25,7 +23,6 @@ public class GuideFrame extends JFrame {
         try {
             setIconImage(ImageIO.read(TitlePanel.class.getResource("/Pictures/Icon.png")));
         } catch (IOException ex) {}
-<<<<<<< HEAD
         StartTurial.setIcon(new ImageIcon("src/Pictures/1.png"));
         StartTurial.setBounds(10, 10, 300, 80);
         add(StartTurial);
@@ -43,10 +40,5 @@ public class GuideFrame extends JFrame {
                 GUIDE.setVisible(true);
             } 
         });
-        
-        
-  //      GUIDE.setVisible(false);
-=======
->>>>>>> d743ceeaef39a635ba55946d575c96c2423bee63
     }
 }
