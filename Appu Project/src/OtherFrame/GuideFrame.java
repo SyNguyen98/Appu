df9af911a9063e500c23085f75d57f3e23e719ec
Tùng Phan Thanh <48848418+thanhtung2105@users.noreply.github.com>
2019-04-18 09:56:29 +0700

    Update GuideFrame.java

--- Appu Project/src/OtherFrame/GuideFrame.java
+++ Appu Project/src/OtherFrame/GuideFrame.java
@@ -1,6 +1,8 @@
 package OtherFrame;
 
 import MainFrame.TitlePanel;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.io.IOException;
 import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
@@ -8,18 +10,39 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 
 public class GuideFrame extends JFrame {
-    private static final JLabel GUIDE = new JLabel();
+    private JLabel GUIDE = new JLabel();
+    private JLabel StartTurial = new JLabel();
+    private JLabel StartImg = new JLabel();
+ //   private static final JButton StarTurial = new JButton();
 
-    public GuideFrame() {
+    public GuideFrame() {  
+        setResizable(false);
         setTitle("Guide");
         setSize(540, 550);
         setLocationRelativeTo(null);
+        setLayout(null);
         try {
             setIconImage(ImageIO.read(TitlePanel.class.getResource("/Pictures/Icon.png")));
         } catch (IOException ex) {}
-        GUIDE.setIcon(new ImageIcon(GuideFrame.class.getResource("/Pictures/1.gif")));
-        GUIDE.setBounds(0, 0, 50, 50);
-        add(GUIDE);
+        StartTurial.setIcon(new ImageIcon("src/Pictures/1.png"));
+        StartTurial.setBounds(10, 10, 300, 80);
+        add(StartTurial);
+        StartImg.setIcon(new ImageIcon("src/Pictures/2.png"));
+        StartImg.setBounds(100, 80, 340, 340);
+        add(StartImg);
+        StartTurial.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                StartTurial.setVisible(false);
+                StartImg.setVisible(false);
+                GUIDE.setIcon(new ImageIcon("src/Pictures/1.gif"));
+                GUIDE.setBounds(10, 30, 500, 400);
+                add(GUIDE);
+                GUIDE.setVisible(true);
+            } 
+        });
+        
+        
   //      GUIDE.setVisible(false);
     }
 }
