de781738420a1033dff0ba9028942f2ac50b16dc
SyNguyen98 <48832411+SyNguyen98@users.noreply.github.com>
2019-04-26 20:57:29 +0700

    Create SuggestiveMenu.java

---
+++ Appu Project/src/Setting/SuggestiveMenu.java
@@ -0,0 +1,31 @@
+package Setting;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import javax.swing.JMenuItem;
+import javax.swing.JPopupMenu;
+
+
+public class SuggestiveMenu extends JPopupMenu {  
+    public SuggestiveMenu() {
+        try (BufferedReader br = new BufferedReader(new FileReader("src/Database/Suggestion.txt"))) {
+            String line = br.readLine(); 
+            while (line != null) {
+                JMenuItem item = new JMenuItem();     
+                item.setOpaque(false);
+                item.setText(line);
+                item.setFont(new Font("Arial", 1, 12));
+                item.setBackground(new Color(0, 0, 51));
+                item.setForeground(Color.WHITE);
+                item.setHorizontalTextPosition(JMenuItem.LEFT);
+                add(item);           
+                line = br.readLine();
+            }
+        } catch (FileNotFoundException ex) {
+        } catch (IOException ex) {}
+    }
+}
