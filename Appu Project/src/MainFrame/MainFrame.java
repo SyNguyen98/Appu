7dd1111badf684af90b8b8ee726d355a437dbe9b
SyNguyen98 <48832411+SyNguyen98@users.noreply.github.com>
2019-04-29 21:03:14 +0700

    Update MainFrame.java

--- Appu Project/src/MainFrame/MainFrame.java
+++ Appu Project/src/MainFrame/MainFrame.java
@@ -2,9 +2,11 @@ package MainFrame;
 
 import Command.Performance;
 import Setting.FrameDrag;
-import Sound.Sound;
+import Setting.Language;
 import java.awt.geom.RoundRectangle2D;
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 import javax.imageio.ImageIO;
@@ -34,28 +36,41 @@ public class MainFrame {
         setIntro();
         try {
             UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-        } catch (ClassNotFoundException | IllegalAccessException | InstantiationException | UnsupportedLookAndFeelException e) {}
+        } catch (ClassNotFoundException | IllegalAccessException | InstantiationException | UnsupportedLookAndFeelException e) {
+        }
 
         try {
             frame.setIconImage(ImageIO.read(new File("src/Pictures/Icon.png")));
-        } catch (IOException ex) {}
+        } catch (IOException ex) {
+        }
         frame.setSize(480, 640);
         frame.setUndecorated(true);
         frame.setShape(new RoundRectangle2D.Double(0, 0, frame.getWidth(), frame.getHeight(), 50, 50));
         frame.setLocationRelativeTo(null);
         frame.setLayout(null);
         frame.setVisible(true);
-        frame.setAlwaysOnTop(true);
+//        frame.setAlwaysOnTop(true);
         frame.addMouseListener(frameDrag);
         frame.addMouseMotionListener(frameDrag);
         try {
             TimeUnit.MILLISECONDS.sleep(3000);
-        } catch (InterruptedException ex) {}
+        } catch (InterruptedException ex) {
+        }
         introLabel.setVisible(false);
         frame.add(inputPanel);
         frame.add(titlePanel);
         frame.add(mainPanel);
-        
+
+//        try (BufferedReader br = new BufferedReader(new FileReader("src/Database/Language.txt"))) {
+//            String language = br.readLine();
+//            if (language.equals("english")) {
+//                Language.setEnglish();
+//            } else {
+//                Language.setVietnamese();
+//            }
+//        } catch (IOException ex) {
+//        }
+
         Performance.Do();
     }
 }
