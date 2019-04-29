8ca6b096246867bafbbb48c9e1d515717da6b5d6
SyNguyen98 <48832411+SyNguyen98@users.noreply.github.com>
2019-04-29 23:25:43 +0700

    Update SuggestiveMenu.java

--- Appu Project/src/Setting/SuggestiveMenu.java
+++ Appu Project/src/Setting/SuggestiveMenu.java
@@ -6,19 +6,20 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import javax.swing.BorderFactory;
 import javax.swing.JMenuItem;
 import javax.swing.JPopupMenu;
 
 
 public class SuggestiveMenu extends JPopupMenu {  
-    public SuggestiveMenu() {
-        try (BufferedReader br = new BufferedReader(new FileReader("src/Database/Suggestion.txt"))) {
+    public SuggestiveMenu(String string) {
+        try (BufferedReader br = new BufferedReader(new FileReader("src/Database/Suggestion" + string + ".txt"))) {
             String line = br.readLine(); 
             while (line != null) {
-                JMenuItem item = new JMenuItem();     
-                item.setOpaque(false);
+                JMenuItem item = new JMenuItem();           
                 item.setText(line);
                 item.setFont(new Font("Arial", 1, 12));
+                item.setOpaque(true);
                 item.setBackground(new Color(0, 0, 51));
                 item.setForeground(Color.WHITE);
                 item.setHorizontalTextPosition(JMenuItem.LEFT);
@@ -27,5 +28,6 @@ public class SuggestiveMenu extends JPopupMenu {
             }
         } catch (FileNotFoundException ex) {
         } catch (IOException ex) {}
+        setBorder(BorderFactory.createEmptyBorder());
     }
 }
