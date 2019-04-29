5bb8962eb5594c89c8dad6ffcacd9b71d26f5adc
SyNguyen98 <48832411+SyNguyen98@users.noreply.github.com>
2019-04-29 19:43:52 +0700

    Update MainFrame.java

--- Appu Project/src/MainFrame/MainFrame.java
+++ Appu Project/src/MainFrame/MainFrame.java
@@ -24,7 +24,7 @@ public class MainFrame {
     private static final JLabel introLabel = new JLabel();
 
     private static void setIntro() {
-        Sound.play("src/Sound/Endgame.wav");
+//        Sound.play("src/Sound/Endgame.wav");
         introLabel.setIcon(new ImageIcon("src/Pictures/Intro.gif"));
         introLabel.setBounds(-30, 0, 600, 640);
         frame.add(introLabel);
