a89156ce4210842caadaaaaaf76376a4ad470bad
SyNguyen98 <48832411+SyNguyen98@users.noreply.github.com>
2019-04-29 21:03:30 +0700

    Update ChangeInfo.java

--- Appu Project/src/Setting/ChangeInfo.java
+++ Appu Project/src/Setting/ChangeInfo.java
@@ -15,9 +15,10 @@ import javax.swing.JFileChooser;
 import javax.swing.JOptionPane;
 
 public class ChangeInfo {
+
     private static final JFileChooser chooser = new JFileChooser("iSekai");
-    
-    private static BufferedImage resize(BufferedImage img, int newW, int newH) { 
+
+    private static BufferedImage resize(BufferedImage img, int newW, int newH) {
         Image tmp = img.getScaledInstance(newW, newH, Image.SCALE_SMOOTH);
         BufferedImage dimg = new BufferedImage(newW, newH, BufferedImage.TYPE_INT_ARGB);
 
@@ -26,28 +27,26 @@ public class ChangeInfo {
         g2d.dispose();
 
         return dimg;
-    } 
-    
+    }
+
     private static String getImagePath() {
         int returnVal = chooser.showOpenDialog(MainFrame.frame);
-        if(returnVal == JFileChooser.APPROVE_OPTION) {        
+        if (returnVal == JFileChooser.APPROVE_OPTION) {
             chooser.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
             return chooser.getSelectedFile().getAbsolutePath();
         }
         return null;
     }
-    
+
     public static void setAvatar() {
-        BufferedImage img = null;
         try {
-                img = ImageIO.read(new File(getImagePath()));
-                ImageIO.write(img, "jpg", new File("src/Pictures/Avatar.jpg"));
-        } catch (IOException e) {}
-        try {
+            BufferedImage img = ImageIO.read(new File(getImagePath()));
+            ImageIO.write(img, "jpg", new File("src/Pictures/Avatar.jpg"));
             TitlePanel.imageLabel.setIcon(CircleLabel.setImageLabel(resize(img, 50, 50)));
-        } catch (IOException e) {}
+        } catch (IOException e) {
+        }
     }
-    
+
     public static void setName() {
         String name = JOptionPane.showInputDialog(null, "Input name :", "Change Name", JOptionPane.INFORMATION_MESSAGE);
         TitlePanel.nameLabel.setText(name);
