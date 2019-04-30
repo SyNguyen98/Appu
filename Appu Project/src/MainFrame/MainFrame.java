f535c37d0bc3101b41735bd68b5141193ede74ba
Tùng Phan Thanh <48848418+thanhtung2105@users.noreply.github.com>
2019-04-30 16:23:28 +0700

    Update MainFrame.java

--- Appu Project/src/MainFrame/MainFrame.java
+++ Appu Project/src/MainFrame/MainFrame.java
@@ -26,7 +26,7 @@ public class MainFrame {
     private static final JLabel introLabel = new JLabel();
 
     private static void setIntro() {
-//        Sound.play("src/Sound/Endgame.wav");
+        Sound.play("src/Sound/Endgame.wav");
         introLabel.setIcon(new ImageIcon("src/Pictures/Intro.gif"));
         introLabel.setBounds(-30, 0, 600, 640);
         frame.add(introLabel);
@@ -60,10 +60,11 @@ public class MainFrame {
  //       frame.setAlwaysOnTop(true);
         frame.addMouseListener(frameDrag);
         frame.addMouseMotionListener(frameDrag);
+        
         try {
             TimeUnit.MILLISECONDS.sleep(3000);
-        } catch (InterruptedException ex) {
-        }
+        } catch (InterruptedException ex) {}
+        
         introLabel.setVisible(false);
         frame.add(inputPanel);
         frame.add(titlePanel);
