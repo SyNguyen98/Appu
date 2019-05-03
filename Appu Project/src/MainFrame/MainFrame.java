57e236dd5504a11d2920dc3c1748d012d4705b9a
SyNguyen98 <48832411+SyNguyen98@users.noreply.github.com>
2019-05-03 10:03:59 +0700

    Update MainFrame.java

--- Appu Project/src/MainFrame/MainFrame.java
+++ Appu Project/src/MainFrame/MainFrame.java
@@ -1,7 +1,12 @@
 package MainFrame;
 
 import Command.Performance;
-import Sound.Theme;
+import Setting.FrameDrag;
+import Sound.Sound;
+import java.awt.geom.RoundRectangle2D;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 import javax.imageio.ImageIO;
@@ -11,40 +16,60 @@ import javax.swing.JLabel;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
 
-public class MainFrame  {
-    public static final JFrame frame = new JFrame("iSekai");
+public class MainFrame {
+
+    public static final JFrame frame = new JFrame();
+    private static final FrameDrag frameDrag = new FrameDrag(frame);
     private static final TitlePanel titlePanel = new TitlePanel();
     private static final MainPanel mainPanel = new MainPanel();
+    private static final InputPanel inputPanel = new InputPanel();
     private static final JLabel introLabel = new JLabel();
-    
+
     private static void setIntro() {
-        Theme.play("src/Sound/Intro.wav");
-        introLabel.setIcon(new ImageIcon(MainFrame.class.getResource("/Pictures/Intro.gif")));
-        introLabel.setBounds(0, 0, 800, 640);
-        frame.add(introLabel);        
+//        Sound.play("src/Sound/Endgame.wav");
+        introLabel.setIcon(new ImageIcon("src/Pictures/Intro.gif"));
+        introLabel.setBounds(-30, 0, 600, 640);
+        frame.add(introLabel);
     }
     
-    public static void main(String[] args) {  
+    public static String getLanguage() {
+        try (BufferedReader br = new BufferedReader(new FileReader("src/Database/Language.txt"))) {
+            String language = br.readLine();
+            return language;
+        } catch (IOException ex) {}
+        return null;
+    }
+
+    public static void main(String[] args) {
         setIntro();
         try {
             UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-        } catch(ClassNotFoundException | IllegalAccessException | InstantiationException | UnsupportedLookAndFeelException e) {}     
-       
+        } catch (ClassNotFoundException | IllegalAccessException | InstantiationException | UnsupportedLookAndFeelException e) {
+        }
+
         try {
-            frame.setIconImage(ImageIO.read(TitlePanel.class.getResource("/Pictures/Icon.png")));
-        } catch (IOException ex) {}   
+            frame.setIconImage(ImageIO.read(new File("src/Pictures/Icon.png")));
+        } catch (IOException ex) {
+        }
         frame.setSize(480, 640);
-        frame.setResizable(false);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setLocationRelativeTo(frame);
-        frame.setVisible(true);       
+        frame.setUndecorated(true);
+        frame.setShape(new RoundRectangle2D.Double(0, 0, frame.getWidth(), frame.getHeight(), 50, 50));
+        frame.setLocationRelativeTo(null);
+        frame.setLayout(null);
+        frame.setVisible(true);
+        frame.setAlwaysOnTop(true);
+        frame.addMouseListener(frameDrag);
+        frame.addMouseMotionListener(frameDrag);
+        
         try {
             TimeUnit.MILLISECONDS.sleep(3000);
         } catch (InterruptedException ex) {}
-        introLabel.setVisible(false);
-        frame.add(mainPanel);
-        frame.add(titlePanel);
         
-        Performance.Do(); 
+        introLabel.setVisible(false);
+        frame.add(inputPanel);
+        frame.add(titlePanel);
+        frame.add(mainPanel);
+            
+        Performance.Do();
     }
 }
