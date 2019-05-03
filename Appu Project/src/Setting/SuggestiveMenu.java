6e599c78d7f7626b4cd460ccfb6d03951daa8d8b
SyNguyen98 <48832411+SyNguyen98@users.noreply.github.com>
2019-05-03 10:56:42 +0700

    Update SuggestiveMenu.java

--- Appu Project/src/Setting/SuggestiveMenu.java
+++ Appu Project/src/Setting/SuggestiveMenu.java
@@ -2,32 +2,42 @@ package Setting;
 
 import java.awt.Color;
 import java.awt.Font;
-import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import javax.swing.BorderFactory;
 import javax.swing.JMenuItem;
 import javax.swing.JPopupMenu;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
+public class SuggestiveMenu extends JPopupMenu {
 
-public class SuggestiveMenu extends JPopupMenu {  
+    private static final JSONParser parser = new JSONParser();
+
     public SuggestiveMenu(String string) {
-        try (BufferedReader br = new BufferedReader(new FileReader("src/Database/Keyword" + string + ".txt"))) {
-            String line = br.readLine(); 
-            while (line != null) {
-                JMenuItem item = new JMenuItem();           
-                item.setText(line);
+        try {
+            JSONArray jsonArray = (JSONArray) parser.parse(new FileReader("src/Database/Keyword.json"));
+            for (Object obj : jsonArray) {
+                JSONObject jsonObject = (JSONObject) obj;
+               
+                JMenuItem item = new JMenuItem();
+                if (string.equals("english"))
+                    item.setText((String) jsonObject.get("Eng"));
+                else
+                    item.setText((String) jsonObject.get("Viet"));
                 item.setFont(new Font("Arial", 1, 12));
                 item.setOpaque(true);
                 item.setBackground(new Color(0, 0, 51));
                 item.setForeground(Color.WHITE);
                 item.setHorizontalTextPosition(JMenuItem.LEFT);
-                add(item);           
-                line = br.readLine();
+                add(item);
+
             }
-        } catch (FileNotFoundException ex) {
-        } catch (IOException ex) {}
+        } catch (FileNotFoundException e) {
+        } catch (IOException | ParseException e) {}
         setBorder(BorderFactory.createEmptyBorder());
     }
 }
