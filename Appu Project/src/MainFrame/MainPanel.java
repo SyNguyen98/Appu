bee7316b1ad86a58a17a159b173689edc656989a
SyNguyen98 <48832411+SyNguyen98@users.noreply.github.com>
2019-05-03 10:04:01 +0700

    Update MainPanel.java

--- Appu Project/src/MainFrame/MainPanel.java
+++ Appu Project/src/MainFrame/MainPanel.java
@@ -2,41 +2,23 @@ package MainFrame;
 
 import Shape.RoundPanel;
 import Shape.CircleLabel;
-import Setting.Autocomplete;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Image;
 import java.awt.image.BufferedImage;
+import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 import javax.imageio.ImageIO;
 import javax.swing.GroupLayout;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
-import javax.swing.JTextField;
-import javax.swing.KeyStroke;
 
-public class MainPanel extends JPanel {
-    public static final JTextField inputField = new Shape.RoundTextField();
+public class MainPanel extends JScrollPane {
     public static JLabel commandLabel, answerLabel, iconLabel, timeLabel;
     public static JPanel commandPanel, answerPanel, mainPanel;
-    public static JScrollPane scrollPane = new JScrollPane();
-    
-    @Override
-    protected void paintComponent(Graphics g) {
-        super.paintComponent(g);
-        BufferedImage image = null;
-        try {                
-            image = ImageIO.read(TitlePanel.class.getResource("/Pictures/Main.jpg"));
-        } catch (IOException ex) {
-            ex.getMessage();
-        }
-        g.drawImage(image, 0, 0, this);         
-    }
     
     private static BufferedImage resize(BufferedImage img, int newW, int newH) { 
         Image tmp = img.getScaledInstance(newW, newH, Image.SCALE_SMOOTH);
@@ -49,13 +31,13 @@ public class MainPanel extends JPanel {
         return dimg;
     } 
     
-    public static void setTime() {
+    public static void setTimeLabel() {
         timeLabel = new JLabel(); 
         timeLabel.setForeground(Color.LIGHT_GRAY);
         timeLabel.setFont(new Font("Arial", 1, 12));
     }
     
-    public static void setCommand() {
+    public static void setCommandPanel() {
         commandPanel = new RoundPanel();
         commandPanel.setLayout(new GroupLayout(commandPanel));
         commandPanel.setOpaque(false);
@@ -67,11 +49,11 @@ public class MainPanel extends JPanel {
         commandPanel.add(commandLabel);
     }
     
-    public static void setAnswer() {
+    public static void setAnswerPanel() {
         iconLabel = new JLabel();
         BufferedImage image = null;
         try {
-            image = ImageIO.read(TitlePanel.class.getResource("/Pictures/Avatar.jpg"));
+            image = ImageIO.read(new File("src/Pictures/Avatar.jpg"));
             iconLabel.setIcon(CircleLabel.setImageLabel(resize(image, 35, 35)));
         } catch (IOException ex) {} 
         answerPanel = new RoundPanel();
@@ -86,59 +68,25 @@ public class MainPanel extends JPanel {
         answerPanel.add(answerLabel);
     }
     
-    public static void setScrollPane() {
+    public MainPanel() {
         mainPanel = new JPanel() {
             @Override
             protected void paintComponent(Graphics g) {
                 super.paintComponent(g);
                 BufferedImage image = null;
                 try {                
-                    image = ImageIO.read(TitlePanel.class.getResource("/Pictures/Main.jpg"));
+                    image = ImageIO.read(new File("src/Pictures/Main.jpg"));
                 } catch (IOException ex) {}
                 g.drawImage(image, 0, 0, this);         
             }
         };
         mainPanel.setBounds(0, 0, 480, 3000);
         mainPanel.setLayout(null);
-        
-        scrollPane.setSize(500, 480);
-        scrollPane.add(mainPanel);
-        scrollPane.setViewportView(mainPanel);
-        scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
-        scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
-    }
-    
-    private static void setInputTextField() {
-        List<String> keywords = new ArrayList<>();
-        keywords.add("video");
-        keywords.add("picture");
-        keywords.add("facebook");
-        keywords.add("music");
-        keywords.add("translate");
-        keywords.add("map");
-        keywords.add("anime");
-        keywords.add("web");
-        keywords.add("open");
-        
-        Autocomplete autoComplete = new Autocomplete(inputField, keywords);
-        
-        inputField.setBounds(20, 490, 370, 50);
-        inputField.setOpaque(false);
-        inputField.setFont(new Font("Arial", 2, 16));
-        inputField.setFocusTraversalKeysEnabled(false);
-        inputField.getDocument().addDocumentListener(autoComplete);
-        inputField.getInputMap().put(KeyStroke.getKeyStroke("TAB"), "commit");
-        inputField.getActionMap().put("commit", autoComplete.new CommitAction());
-    }
-    
-    public MainPanel() {
-        setInputTextField();
-        add(inputField);
-        
-        setScrollPane();
-        add(scrollPane);
 
-        setLayout(new GroupLayout(this));
-        setBounds(0, 60, 480, 580);   
+        setBounds(0, 60, 500, 500);
+        setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+        setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+        add(mainPanel);
+        setViewportView(mainPanel);   
     }
 }
