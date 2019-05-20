e6884cc01fd88b42063e71411ae6647527e38410
SyNguyen98 <48832411+SyNguyen98@users.noreply.github.com>
2019-05-21 00:14:59 +0700

    Update Performance.java

--- Appu Project/src/Command/Performance.java
+++ Appu Project/src/Command/Performance.java
@@ -5,7 +5,6 @@ import Database.VietSQL;
 import MainFrame.InputPanel;
 import MainFrame.MainFrame;
 import MainFrame.MainPanel;
-import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.text.SimpleDateFormat;
@@ -15,39 +14,24 @@ public class Performance {
 
     public static boolean online;
 
-    private static void setTime() {
-        MainPanel.setTimeLabel();
-        MainPanel.timeLabel.setText(new SimpleDateFormat("HH:mm").format(Calendar.getInstance().getTime()));
-        MainPanel.timeLabel.setBounds(225, 0, 50, 40);
-    }
-
     private static void setCommand(String command, int width, int height) {
-        MainPanel.setCommandPanel();
         MainPanel.commandPanel.setBounds(260 + (180 - width), 20 + height / 2, width + 20, height + 10);
         MainPanel.commandLabel.setText("<html>" + command.replace("@", "<br>") + "</html>");
         MainPanel.commandLabel.setBounds(10, 5, width, height);
     }
 
     private static void setAnswer(String answer, int width, int height) {
-        MainPanel.setAnswerPanel();
         MainPanel.iconLabel.setBounds(10, MainPanel.commandPanel.getHeight() + 37, 35, 35);
         MainPanel.answerPanel.setBounds(55, MainPanel.commandPanel.getHeight() + 35, width + 20, height + 10);
         MainPanel.answerLabel.setText("<html>" + answer.replace("@", "<br>") + "</html>");
         MainPanel.answerLabel.setBounds(10, 5, width, height);
     }
 
-    static void setMainPanel() {
-        MainFrame.mainPanel.add(MainPanel.timeLabel);
-        MainFrame.mainPanel.add(MainPanel.commandPanel);
-        MainFrame.mainPanel.add(MainPanel.answerPanel);
-        MainFrame.mainPanel.add(MainPanel.iconLabel);
-    }
-
     public static void Do() {
         InputPanel.inputField.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent ae) {
-                setTime();
+                MainPanel.timeLabel.setText(new SimpleDateFormat("HH:mm").format(Calendar.getInstance().getTime()));
 
                 String command = Proccess.makeNewLine(InputPanel.inputField.getText());
                 int width = Proccess.width;
@@ -65,8 +49,6 @@ public class Performance {
                 width = Proccess.width;
                 height = Proccess.height;
                 setAnswer(answer, width, height);
-
-                setMainPanel();
 
                 online = true;
                 Computer.controlComputer();
