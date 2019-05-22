55cb60c583267656daf264f78c56cdd1f643a8fd
Tùng Phan Thanh <48848418+thanhtung2105@users.noreply.github.com>
2019-05-22 16:14:16 +0700

    Update Performance.java

--- Appu Project/src/Command/Performance.java
+++ Appu Project/src/Command/Performance.java
@@ -5,7 +5,6 @@ import Database.VietSQL;
 import MainFrame.InputPanel;
 import MainFrame.MainFrame;
 import MainFrame.MainPanel;
-import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.text.SimpleDateFormat;
@@ -14,43 +13,25 @@ import java.util.Calendar;
 public class Performance {
 
     public static boolean online;
-    private static int length = 0;
-
-    private static void setTime() {
-        MainPanel.setTimeLabel();
-        MainPanel.timeLabel.setText(new SimpleDateFormat("HH:mm").format(Calendar.getInstance().getTime()));
-        MainPanel.timeLabel.setBounds(225, length, 50, 40);
-    }
 
     private static void setCommand(String command, int width, int height) {
-        MainPanel.setCommandPanel();
-        MainPanel.commandPanel.setBounds(260 + (180 - width), 20 + height / 2 + length, width + 20, height + 10);
+        MainPanel.commandPanel.setBounds(260 + (180 - width), 20 + height / 2, width + 20, height + 10);
         MainPanel.commandLabel.setText("<html>" + command.replace("@", "<br>") + "</html>");
         MainPanel.commandLabel.setBounds(10, 5, width, height);
     }
 
     private static void setAnswer(String answer, int width, int height) {
-        MainPanel.setAnswerPanel();
-        MainPanel.iconLabel.setBounds(10, MainPanel.commandPanel.getHeight() + 37 + length, 35, 35);
-        MainPanel.answerPanel.setBounds(55, MainPanel.commandPanel.getHeight() + length + 35, width + 20, height + 10);
+        MainPanel.iconLabel.setBounds(10, MainPanel.commandPanel.getHeight() + 37, 35, 35);
+        MainPanel.answerPanel.setBounds(55, MainPanel.commandPanel.getHeight() + 35, width + 20, height + 10);
         MainPanel.answerLabel.setText("<html>" + answer.replace("@", "<br>") + "</html>");
         MainPanel.answerLabel.setBounds(10, 5, width, height);
-    }
-
-    static void setMainPanel() {
-        MainPanel.mainPanel.setPreferredSize(new Dimension(MainPanel.mainPanel.getWidth(), MainPanel.mainPanel.getHeight() + MainPanel.commandPanel.getHeight()));
-        MainPanel.mainPanel.add(MainPanel.timeLabel);
-        MainPanel.mainPanel.add(MainPanel.commandPanel);
-        MainPanel.mainPanel.add(MainPanel.answerPanel);
-        MainPanel.mainPanel.add(MainPanel.iconLabel);
-        MainPanel.mainPanel.revalidate();
     }
 
     public static void Do() {
         InputPanel.inputField.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent ae) {
-                setTime();
+                MainPanel.timeLabel.setText(new SimpleDateFormat("HH:mm").format(Calendar.getInstance().getTime()));
 
                 String command = Proccess.makeNewLine(InputPanel.inputField.getText());
                 int width = Proccess.width;
@@ -69,8 +50,6 @@ public class Performance {
                 height = Proccess.height;
                 setAnswer(answer, width, height);
 
-                setMainPanel();
-
                 online = true;
                 Computer.controlComputer();
 
@@ -81,7 +60,6 @@ public class Performance {
                     Internet.accessInternet();
 
                 InputPanel.inputField.setText("");
-                length += MainPanel.timeLabel.getHeight() + MainPanel.commandPanel.getHeight() + MainPanel.answerPanel.getHeight() + 10;
             }
         });
 
