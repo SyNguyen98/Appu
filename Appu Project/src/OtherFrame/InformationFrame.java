e0abcb8a39392c68f34f6fd9e390a40a95fd6454
SyNguyen98 <48832411+SyNguyen98@users.noreply.github.com>
2019-05-28 02:13:38 +0700

    Update InformationFrame.java

--- Appu Project/src/OtherFrame/InformationFrame.java
+++ Appu Project/src/OtherFrame/InformationFrame.java
@@ -66,14 +66,14 @@ public class InformationFrame extends JFrame {
         exitButton.setBounds(110, 260, 80, 30);
         exitButton.setOpaque(false);
         exitButton.setBackground(new Color(0, 0, 51));
-        exitButton.setForeground(Color.WHITE);
-        add(exitButton);
+        exitButton.setForeground(Color.WHITE);       
         exitButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent ae) {
                 setVisible(false);
             }
         });
+        add(exitButton);
     }
 
     private void setIcon() {
@@ -84,6 +84,7 @@ public class InformationFrame extends JFrame {
         } catch (IOException ex) {}
         iconLabel.setBounds(125, 10, 60, 60);
         add(iconLabel);
+        
         versionLabel = new JLabel();
         versionLabel.setBounds(105, iconLabel.getHeight() + 5, 130, 30);
         versionLabel.setFont(new Font("Arial", 3, 14));
@@ -121,23 +122,23 @@ public class InformationFrame extends JFrame {
         setInfo();
 
         if ("english".equals(MainFrame.getLanguage())) {
-            versionLabel.setText("Version 2.1.7");
+            versionLabel.setText("Version 3.0");
             infoLabel.setText("<html>   i.Sekai is a multimedia virtual assistant developed by a mystery anomyous team, written in Java."
                     + " Software's name has inspiration from the meaning 'The World' in Japanese, compline with 'i' to create"
                     + " a new convinient and modern world for almost users on Windows OS. It can make not only an impression in the way using computer"
                     + " but also change the experience when you surfing Internet. Let's try it once, you will lust to use it again!"
                     + "</html>");
-            infoLabel1.setText("<html> Lastest updated on 29th April 2019. </html>");
+            infoLabel1.setText("<html> Lastest updated on 29th May 2019. </html>");
             exitButton.setText("Exit");
         } else {
-            versionLabel.setText("Phiên bản 2.1.7");
+            versionLabel.setText("Phiên bản 3.0");
             infoLabel.setText("<html>    i.Sekai là trợ lý ảo đa chức năng được phát triển bởi một nhóm bí ẩn, viết bằng ngôn ngữ Java."
                     + " Tên của phần mềm được lấy cảm hứng từ từ 'Thế giới' trong tiếng Nhật, kết hợp với 'i' tạo nên một 'Thế giới ảo' hiện đại,"
                     + " tiện dụng cho người dùng trên hệ điều hành Windows. Nó không chỉ thay đổi ấn tượng cách sử dụng máy tính thông thường mà còn"
                     + " mang đến trải nghiệm mới lạ khi bạn sử dụng đa phương tiện thông qua Internet. Hãy thử sử dụng i.Sekai một lần, rồi bạn sẽ"
                     + " muốn trải nghiệm nó thêm nhiều lần nữa!"
                     + "</html>");
-            infoLabel1.setText("<html>Cập nhật gần nhất: 29 tháng 4, 2019. </html>");
+            infoLabel1.setText("<html>Cập nhật gần nhất: 29 tháng 5, 2019. </html>");
             exitButton.setText("Thoát");
         }
     }
